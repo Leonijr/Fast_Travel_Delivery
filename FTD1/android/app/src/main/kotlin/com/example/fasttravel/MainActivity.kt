26bd60d1900374afe4aafbd7004ad5ec1d1a52df
Leonijr <leoniaguiarjunior@gmail.com>
2022-05-25 21:03:19 -0400

    FTD Alpha 1.0

--- FTD1/android/app/src/main/kotlin/com/example/fasttravel/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.leojr.fasttravel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ FastTravelDelivery/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:fasttravel/login/login_screen.dart';
+import 'package:fasttravel/screens/cliente/client_Screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      initialRoute: '/log',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/log': ((context) => const loginScreen()),
+        '/addClient': ((context) => const addClient()),
+      },
+    );
+  }
+}
